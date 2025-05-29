f27be753529e0b5b8e881c866a367f453c91a7da
Omik Acharya <omikacharya@gmail.com>
2025-05-30 01:17:06 +0530

    version: 1.1

--- friendsAC.js
+++ friendsAC.js
@@ -1,18 +1,25 @@
 document.addEventListener("keydown", (event) => {
-  console.log("Started");
-  console.log(event.code);
   if (event.code === "KeyG") {
-    console.log("Triggered");
     const url = window.location.href;
-    const match = url.match(
-      /codeforces\.com\/(?:contest|problemset\/problem|gym)\/(\d+)\/([A-Z]\d*)/i,
-    );
+    const isContestpage = url.includes("contest/");
+    const isGymPage = url.includes("gym/");
+    let match = null;
+
+    if (isContestpage) {
+      match = url.match(/codeforces\.com\/contest\/(\d+)\/problem\/([A-Z]\d*)/i);
+    } else if (isGymPage) {
+      match = url.match(/codeforces\.com\/gym\/(\d+)\/problem\/([A-Z]\d*)/i);
+    } else {
+      match = url.match(/codeforces\.com\/problemset\/problem\/(\d+)\/([A-Z]\d*)/i);
+    }
 
     if (match) {
-      const contestId = match[1];
+      const contestID = match[1];
       const problemIndex = match[2];
-
-      const friendsUrl = `https://codeforces.com/contest/${contestId}/status?friends=on&status=OK&handle=&problemIndex=${problemIndex}`;
+      let friendsUrl = `https://codeforces.com/contest/${contestID}/status?friends=on&status=OK&handle=&problemIndex=${problemIndex}`;
+      if (isGymPage) {
+        friendsUrl = `https://codeforces.com/gym/${contestID}/status?friends=on&status=OK&handle=&problemIndex=${problemIndex}`;
+      }
       window.open(friendsUrl, "_blank");
     }
   }
